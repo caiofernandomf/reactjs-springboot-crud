ca88d6d6abdbfebf1be2c27790f28c154b2614d3
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-10 14:26:27 -0300

    Adicionando código html ao HeaderCompnent

--- react-frontend/src/components/HeaderComponent.jsx
+++ react-frontend/src/components/HeaderComponent.jsx
@@ -5,13 +5,21 @@ class HeaderComponent extends Component {
         super(props);
 
         this.state = {
-            
+
         }
     }
     render() {
         return (
             <div>
-                
+               <header>
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                        <div>
+                             <a href="https://github.com/caiofernandomf" className="navbar-brand">
+                                 Employee Managment
+                             </a>
+                        </div>
+                    </nav>
+                </header> 
             </div>
         );
     }
