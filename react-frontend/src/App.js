f33674fc87b8831d919b54df0520da4957ae56eb
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-10 14:37:29 -0300

    Adicionando o HeaderComponent a aplicação

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
 
 function App() {
   return (
-    <div className="container">
-          <ListEmployeeComponent/>      
+    <div>
+        <div>
+          <HeaderComponent/>
+        </div>
+        <div className="container">
+            <ListEmployeeComponent/>      
+        </div>
     </div>
+    
   );
 }
 
