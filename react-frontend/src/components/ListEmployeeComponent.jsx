d8f0dddfecf8b9e2fe26b9838832cac7f4963dd7
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-11 00:23:17 -0300

    Adicionando botão Add employee

--- react-frontend/src/components/ListEmployeeComponent.jsx
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -8,16 +8,27 @@ class ListEmployeeComponent extends Component {
         this.state = {
                 employees : []
         }
+
+        this.addEmployee = this.addEmployee.bind(this);
     }
     componentDidMount(){
         EmployeeService.getEmployees().then((res) =>{
             this.setState({employees: res.data});
         });
     }
+
+    addEmployee(){
+        this.props.history.push('/add-employee');
+    }
     render() {
         return (
             <div>
                 <h2 className="text-center">Employees Lis</h2>
+                <div className="row">
+                    <button className="btn btn=primary" onClick={this.addEmployee}>
+                        Add Employee
+                    </button>
+                </div>
                 <div className="row">
                         <table className="table table-striped table-borderd">
                             <thead>
