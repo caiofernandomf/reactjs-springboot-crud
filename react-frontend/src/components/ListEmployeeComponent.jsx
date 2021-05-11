199d35604bb156777cb05b5105648f937fd47bf6
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-11 01:12:55 -0300

    Adicionando botao Add employee

--- react-frontend/src/components/ListEmployeeComponent.jsx
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -25,7 +25,7 @@ class ListEmployeeComponent extends Component {
             <div>
                 <h2 className="text-center">Employees List</h2>
                 <div className="row">                    
-                        <button className="btn btn-primary" onClick={this.addEmployee}>Add Employee</button>                    
+                    <button className="btn btn-primary" onClick={this.addEmployee}>Add Employee</button>                    
                 </div>
                 <div className="row">
                         <table className="table table-striped table-borderd">
