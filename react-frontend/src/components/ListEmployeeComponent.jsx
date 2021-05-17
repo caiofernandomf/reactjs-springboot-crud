25196d4dd20945ea15c3ac63f7a4a3588669d5a0
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-17 14:03:26 -0300

    Adicionando chamada para tela de atualização

--- react-frontend/src/components/ListEmployeeComponent.jsx
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -10,6 +10,10 @@ class ListEmployeeComponent extends Component {
         }
 
         this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+    }
+    editEmployee(id){
+        this.props.history.push('/update-employee/'+id);
     }
     componentDidMount(){
         EmployeeService.getEmployees().then((res) =>{
@@ -47,6 +51,10 @@ class ListEmployeeComponent extends Component {
                                             <td>{employee.firstName}</td>
                                             <td>{employee.lastName}</td>
                                             <td>{employee.emailId}</td>
+                                            <td>
+                                                <button onClick={() => this.editEmployee(employee.id)}
+                                                className="btn btn-info">Update</button>
+                                            </td>
                                         </tr>
                                     )
                                 }
