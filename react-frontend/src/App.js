594084720c0d5b015683acdb2e06df28ac83dd30
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-17 14:01:53 -0300

    Adicionando Rota para atualizar employee

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,17 +5,19 @@ import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
 
 function App() {
   return (
     <div>
         <Router>          
-            <HeaderComponent/>          
+            <HeaderComponent/>
             <div className="container">
                 <Switch>
                     <Route path="/" exact component={ListEmployeeComponent}></Route>
                     <Route path="/employees" component={ListEmployeeComponent}></Route>
-                    <Route path="/add-employee" component={CreateEmployeeComponent}></Route>                  
+                    <Route path="/add-employee" component={CreateEmployeeComponent}></Route>
+                    <Route path="/update-employee/:id" component={UpdateEmployeeComponent}></Route>
                 </Switch>
             </div>
             <FooterComponent/>          
