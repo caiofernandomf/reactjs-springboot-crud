b7bfdebc3416a892ca673a3438852283e1464186
Caio Figueiredo <caiofernandomf@gmail.com>
2021-05-22 19:24:34 -0300

    Adicionando form ao ViewEmployeeComponent

--- react-frontend/src/components/ViewEmployeeComponent.jsx
+++ react-frontend/src/components/ViewEmployeeComponent.jsx
@@ -22,39 +22,45 @@ class ViewEmployeeComponent extends React.Component {
         return (
             <div>
                 <br />
-                <div className="card col-md-6 offset-md-3">
-                <h3 className="text-center">View Employee Detais</h3>
-                <div className="card-body">
+                <div className="container">
                     <div className="row">
-                        <div className="form-group">
-                            <label htmlFor="">Employee First Name: </label>
-                            <div>
-                                <input readOnly="true" type="text" placeholder="First Name" name="firstName" className="form-control"
-                                            value={this.state.employee.firstName} />
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                            <h3 className="text-center">View Employee Detais</h3>
+                            <div className="card-body">
+                                <form action="">
+                                    {/* <div className="row"> */}
+                                        <div className="form-group">
+                                            <label htmlFor="">Employee First Name: </label>
+                                            <div>
+                                                <input readOnly="true" type="text" placeholder="First Name" name="firstName" className="form-control"
+                                                            value={this.state.employee.firstName} />
+                                            </div>
+                                        </div>    
+                                    {/* </div> */}
+                                    {/* <div className="row"> */}
+                                        <div className="form-group">
+                                            <label htmlFor="">Employee Last Name: </label>
+                                            <div>
+                                                <input readOnly="true" type="text" placeholder="Last Name" name="lastName" className="form-control"
+                                                            value={this.state.employee.lastName} />
+                                            </div>
+                                        </div>    
+                                    {/* </div> */}
+                                    {/* <div className="row"> */}
+                                        <div className="form-group">
+                                            <label htmlFor="">Employee Email Id: </label>
+                                            <div>
+                                                <input readOnly="true" type="text" placeholder="First Name" name="firstName" className="form-control"
+                                                            value={this.state.employee.emailId} />
+                                            </div>
+                                        </div>    
+                                    {/* </div> */}
+                                </form>
+                                
                             </div>
-                        </div>    
+                        </div>
                     </div>
-                    <div className="row">
-                        <div className="form-group">
-                            <label htmlFor="">Employee Last Name: </label>
-                            <div>
-                                <input readOnly="true" type="text" placeholder="Last Name" name="lastName" className="form-control"
-                                            value={this.state.employee.lastName} />
-                            </div>
-                        </div>    
-                    </div>
-                    <div className="row">
-                        <div className="form-group">
-                            <label htmlFor="">Employee Email Id: </label>
-                            <div>
-                                <input readOnly="true" type="text" placeholder="First Name" name="firstName" className="form-control"
-                                            value={this.state.employee.emailId} />
-                            </div>
-                        </div>    
-                    </div>
-                    
-                </div>
-            </div>    
+                </div>    
             </div>
         );
     }
